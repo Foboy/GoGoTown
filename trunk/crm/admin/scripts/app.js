4497b3f2beb4d2d13e09927a2fd68fed055e9599
xcoolray <285025600@qq.com>
2014-03-25 16:33:36 +0800

    admin

---
+++ trunk/crm/admin/scripts/app.js
@@ -0,0 +1,68 @@
+﻿angular.module('gogotowncrm', ['ngRoute', 'ui.router', 'ngRestUrls']).
+ config(['$provide', '$httpProvider', '$routeProvider', '$stateProvider', '$urlRouterProvider', function ($provide, $httpProvider, $routeProvider, $stateProvider, $urlRouterProvider) {
+
+     $routeProvider
+        .when('/home', { template: '', controller: function () { } })
+        .when('/boss', { template: '', controller: function () { } })
+        .when('/user', { template: '', controller: function () { } })
+        .when('/mytimeshaft', { template: '', controller: function () { } })
+        .when('/staffmangement', { template: '', controller: function () { } })
+        .when('/changepassword', { template: '', controller: function () { } })
+        .when('/changeemail', { template: '', controller: function () { } })
+        .when('/enterpriseinfo', { template: '', controller: function () { } })
+        .when('/product/:catalogId?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/sales/:steps?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/finance/:steps?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/client/:sorts?/:pageIndex?', { template: '', controller: function () { } })
+        .otherwise({ redirectTo: '/home' });
+
+     $stateProvider
+         .state("main", {
+             url: "",
+             templateUrl: 'partials/main.html'
+         })
+         .state("boss", {
+             url: "^/boss",
+             templateUrl: 'partials/boss.html',
+             controller: UserBossMainCtrl
+         })
+         .state('main.home', {
+             url: '/home',
+             templateUrl: 'partials/home.html',
+             controller: HomeMainCtrl
+         })
+         .state("main.product", {
+             url: "/product*path",
+             templateUrl: 'partials/product.html',
+             controller: ProductMainCtrl
+         })
+         .state('main.user', { url: '/user*path', templateUrl: 'partials/userinfo.html', controller: UserMainCtrl })
+         .state('main.mytimeshaft', { url: '/mytimeshaft*path', templateUrl: 'partials/mytimeshaft.html', controller: UserTimeShaftCtrl })
+         .state('main.staffmangement', { url: '/staffmangement*path', templateUrl: 'partials/staffmangement.html', controller: StaffMangementCtrl })
+         .state('main.changepassword', { url: '/changepassword*path', templateUrl: 'partials/changepassword.html', controller: UserMainCtrl })
+         .state('main.changeemail', { url: '/changeemail*path', templateUrl: 'partials/changeemail.html', controller: UserMainCtrl })
+         .state('main.enterpriseinfo', { url: '/enterpriseinfo*path', templateUrl: 'partials/enterpriseinfo.html', controller: EnterPriseInfoCtrl })
+         .state('main.sales', { url: '/sales*path', templateUrl: 'partials/sales.html', controller: SalesMainCtrl })
+         .state('main.finance', { url: '/finance*path', templateUrl: 'partials/finance.html', controller: FinanceMainCtrl })
+         .state('main.client', { url: '/client*path', templateUrl: 'partials/client.html', controller: ClientMainCtrl });
+
+     $httpProvider.interceptors.push(function () {
+         return {
+             'response': function (response) {
+                 if (response && typeof response.data === 'object') {
+                     if (response.data.Error == 11) {
+                         setTimeout(function () { window.location.href = 'login.html'; }, 3000);
+                     }
+                 }
+                 return response || $q.when(response);
+             }
+         };
+     });
+ }])
+    .value('$anchorScroll', angular.noop)
+    .run(
+      ['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;
+      }]);;
---
+++ trunk/crm/admin/scripts/rest-urls.js
@@ -0,0 +1,25 @@
+﻿/*
+**  Create by foboy.cray
+**  2014/03/25
+*/
+(function (window, angular, undefined) {
+    'use strict';
+    angular.module('ngRestUrls', ['ng']).
+      config(['$provide', function ($provide) {
+          var resturls = {};
+          resturls.base = "";
+
+          resturls.add = function (name, url) {
+              resturls[name] = resturls.base + url + "?timestamp=" + new Date().getTime();
+          };
+
+          resturls.addpage = function (name, url) {
+              resturls[name] = resturls.base + url + "?timestamp=" + new Date().getTime();
+          };
+
+          resturls.addpage("login_page", "login.php");
+
+          $provide.constant('$resturls', resturls);
+
+      }]);
+})(window, window.angular);
