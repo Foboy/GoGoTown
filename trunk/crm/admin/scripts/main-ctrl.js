365a8f66c0306d1b87ab28e95807bf0d4f6427fe
zhengrunqiang <653260669@qq.com>
2014-04-02 22:28:03 +0800

    login.html

--- trunk/crm/admin/scripts/main-ctrl.js
+++ trunk/crm/admin/scripts/main-ctrl.js
@@ -1,13 +1,15 @@
 ﻿function MainCtrl($scope, $routeParams, $http, $location, $filter, $resturls) {
     //登录
     $scope.UserLogin = function (user) {
+        $scope.errormessage = false;
         if ($scope.LoginForm.$valid) {
             $scope.showerror = false;
             $http.post($resturls["Login"], { user_name: user.user_name, user_password: user.user_password }).success(function (data) {
-                if (data.Error) {
+                if (data.Data) {
                     window.location.href = "index.html";
                 } else {
-                    window.location.href = "index.html";
+                    $scope.showerror = true;
+                    $scope.errormessage = true;
                 }
             }).error(function (data, status, headers, config) {
                 alert('error');
