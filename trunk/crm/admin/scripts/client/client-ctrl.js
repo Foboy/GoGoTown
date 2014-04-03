cd21f37c12d16261e4bf6f36aecf58cb3ef353bf
zhengrunqiang <653260669@qq.com>
2014-04-03 23:27:20 +0800

    xiugai

--- trunk/crm/admin/scripts/client/client-ctrl.js
+++ trunk/crm/admin/scripts/client/client-ctrl.js
@@ -3,44 +3,37 @@ function ClientMainCtrl($scope, $http, $location, $routeParams, $resturls) {
     $scope.sorts = $routeParams.sorts;
     if (!$scope.sorts) {
         $scope.sorts = "owncustomer";
-
-        //$parent.enterpriseActPageIndex = 1;
     } //gogo客户
     else {
-        // $parent.personalActPageIndex = 1;
+       
     }
-    $scope.loadCurrentSortList = function (sorts,pageIndex) {
+    $scope.loadClientSortList = function () {
         // if (pageIndex == 0) pageIndex = 1;
-        switch (sorts) {
-            case '#gogocustomer': 
-                $http.post($resturls["LoadGoGoCustomerList"], {name:'',phone:'',sex:0,type:0, pageindex: pageIndex  }).success(function (data) {
-                    if (data.Error!=0) {
-                        alert(data.ErrorMessage, 'e');
+        switch ($scope.sorts) {
+            case 'owncustomer':
+                $http.post($resturls["LoadOwnCustomersList"], { name: '', phone: '', sex: 0, pageindex: 0 }).success(function (result) {
+                    console.log(result);
+                    if (result.Error == 0) {
+                        $scope.ownclients = result.Data;
                     } else {
-                        $scope.enterpriseclients = data.Data;
-                        //console.log($scope.enterpriseclients);
-                        $parent.enterpriseActPageIndex = pageIndex;
-                        //$parent.pages = utilities.paging(data.Data.RecordsCount, pageIndex, 10, '#client/' + $scope.sorts + '/{0}');
+                        $scope.ownclients = [];
                     }
-                }).error(function (data, status, headers, config) {
-                    $scope.enterpriseclients = [];
-                })
+                });
                 break;
-            case '#owncustomer': 
-                $http.post($resturls["LoadOwnCustomersList"], { name:'',phone:'',sex:0,pageindex: pageIndex }).success(function (data) {
-                    if (data.Error!=0) { alert(data.ErrorMessage, 'e'); } else {
-                    	console.log(data.Data)
-                        $scope.personalclients = data.Data;
-                      
-                        $parent.personalActPageIndex = pageIndex;
-                        //$parent.pages = utilities.paging(data.Data.RecordsCount, pageIndex, 10, '#client/' + $scope.sorts + '/{0}');
+            case 'gogocustomer':
+                $http.post($resturls["LoadGoGoCustomerList"], { name: '', phone: '', sex: 0, type: 0, pageindex: 0 }).success(function (result) {
+                    console.log(result);
+                    if (result.Error == 0) {
+                        $scope.gogoclients = result.Data;
+                    } else {
+                        $scope.gogoclients = [];
                     }
-                }).error(function (data, status, headers, config) {
-                    $scope.personalclients = [];
-                })
+                });
                 break;
+            
         }
     }
+    $scope.loadClientSortList();
     $scope.load = function () {
         console.log("Call clientcontroller");
     }
@@ -48,22 +41,28 @@ function ClientMainCtrl($scope, $http, $location, $routeParams, $resturls) {
     $scope.ShowAddOwnCustomerModal = function () {
         $("#addcustomermodal").modal('show');
     }
-    $scope.AddOwnCustomerSubmit = function (User) {
+    
+}
+
+function AddOwnCustomerCtrl($scope, $http, $location, $routeParams, $resturls)
+{
+    $scope.SaveAddOwnCustomer = function (data) {
         if ($scope.AddOwnCustomerForm.$valid) {
             $scope.showerror = false;
-            $http.post($resturls["LoadGoGoCustomerList"], {}).success(function (data) {
-                if (data.Error) {
-                    alert(data.ErrorMessage, 'e');
+            $http.post($resturls["AddOwnCustomer"], { name: data.name, sex: data.sex, phone: data.phone, birthday: data.birthday, remark: data.remark }).success(function (data) {
+                if (data.Error == 0) {
+                    $("#addcustomermodal").modal('hide');
+                    alert("success");
                 }
                 else {
-
+                    alert("e");
                 }
             })
-        }
-        else {
+    }
+else {
             $scope.showerror = true;
-        }
-    };
+}
+};
 }
 
 function SeaCustomerMainCtrl($scope, $http, $location, $routeParams, $resturls) {
