da4d3968dc81b3606a5847a3c3b105af8e3555e2
wangxiao <yuwojiusi@163.com>
2014-04-11 10:55:45 +0800

    更新弹出框样式

--- trunk/crm/admin/scripts/rest-urls.js
+++ trunk/crm/admin/scripts/rest-urls.js
@@ -12,7 +12,7 @@
           //resturls.base = "http://localhost:8080/GoGoTown/trunk/crm/index.php";
           //resturls.base = "http://localhost/GoGoTown/trunk/crm/index.php";
          //resturls.base = "http://localhost/index.php";
-          resturls.base = "http://localhost:8080/GoGoTown/trunk/crm/index.php";
+          //resturls.base = "http://localhost:8080/GoGoTown/trunk/crm/index.php";
           resturls.add = function (name, url) {
               resturls[name] = resturls.base + "?url=" + url;
           };
