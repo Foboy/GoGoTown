7100461f2fdb7c1efeac705e82779e3095a96779
wangxiao <yuwojiusi@163.com>
2014-04-23 10:27:55 +0800

    更新消费记录样式

--- trunk/crm/admin/scripts/rest-urls.js
+++ trunk/crm/admin/scripts/rest-urls.js
@@ -8,7 +8,7 @@
       config(['$provide', function ($provide) {
           var resturls = {};
           //resturls.base = "http://localhost:8080/GoGoTown/trunk/crm/index.php";
-         resturls.base = "http://192.168.1.7/index.php";
+         resturls.base = "http://192.168.123.30/index.php";
          // resturls.base = "/crm/index.php";
           resturls.add = function (name, url) {
               resturls[name] = resturls.base + "?url=" + url;
