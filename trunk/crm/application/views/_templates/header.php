f6de9b6e492940f2b1a459cf2c2398e9287063ac
xcoolray <285025600@qq.com>
2014-06-26 22:43:36 +0800

    header

--- trunk/crm/application/views/_templates/header.php
+++ trunk/crm/application/views/_templates/header.php
@@ -3,15 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>My Application</title>
+    <title>GOGO商家CRM系统</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS -->
-    <link rel="stylesheet" href="<?php echo SURL; ?>public/css/reset.css" />
-    <link rel="stylesheet" href="<?php echo SURL; ?>public/css/style.css" />
-    <!-- in case you wonder: That's the cool-kids-protocol-free way to load jQuery -->
-    <script type="text/javascript" src="//code.jquery.com/jquery-2.0.3.min.js"></script>
-    <script type="text/javascript" src="
-      <?php echo SURL; ?>public/js/application.js"></script>
 </head>
 <body>
